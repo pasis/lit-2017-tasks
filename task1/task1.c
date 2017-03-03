5647dd11c7612782e53634eac0258ec3d3f0a1d2
Dmitry Podgorny <pasis.ua@gmail.com>
2017-03-03 22:40:14 +0200

    task1: fixed error, improved
    - Fixed error: opening '|' could be treated as closing
    - Added brackets []
    - Improved brackets sequence validation
    - eval() returns an error on division by zero or sqrt of negative number
    - scanf() replaced with gets()

--- task1/task1.c
+++ task1/task1.c
@@ -6,17 +6,44 @@
 /* gcc -lm -o task1 task1.c */
 
 #include <assert.h>	/* assert */
-#include <math.h>	/* pow, sqrt */
+#include <ctype.h>	/* tolower */
+#include <math.h>	/* pow, sqrt, fabs, trunc */
 #include <stdbool.h>	/* bool */
-#include <stdio.h>	/* scanf, printf */
+#include <stdio.h>	/* gets, printf */
 #include <stdlib.h>	/* strtod */
 #include <string.h>	/* strlen, strncmp */
-#include <unistd.h>	/* exit */
 
-#define N 255
+#define N 255 /* XXX */
 
-static double calc(const char *s, int *offset);
+static double calc(const char *s, int *offset, bool *error);
 
+static bool is_op(char c)
+{
+	return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
+}
+
+static bool is_closing(const char *s, int i)
+{
+	if (s[i] == '|') {
+		for (; s[i] == '|'; ++i);
+		return s[i] == '\0' || is_op(s[i]) || is_closing(s, i);
+	} else
+		return s[i] == ')' || s[i] == ']';
+}
+
+static bool is_opening(const char *s, int i)
+{
+	return s[i] == '(' || s[i] == '[' || (s[i] == '|' && !is_closing(s, i));
+}
+
+static bool is_same(char c1, char c2)
+{
+	return (c1 == '(' && c2 == ')') ||
+	       (c1 == '[' && c2 == ']') ||
+	       (c1 == '|' && c2 == '|');
+}
+
+/* Removes spaces and converts letters to lower case. */
 static void normalize(char *s)
 {
 	int i;
@@ -24,24 +51,36 @@ static void normalize(char *s)
 
 	for (i = 0, j = 0; s[i] != '\0'; ++i)
 		if (s[i] != ' ')
-			s[j++] = s[i];
+			s[j++] = tolower(s[i]);
 	s[j] = '\0';
 }
 
+/* Checks if brackets sequence is valid. */
 static bool brackets_seq_is_valid(const char *s)
 {
-	size_t len = strlen(s);
-	int    c   = 0;
-	int    i;
+	size_t  len = strlen(s);
+	char   *stack;
+	int     top = 0;
+	int     i;
 
-	for (i = 0; i < len && c >= 0; ++i)
-		if (s[i] == '(')
-			++c;
-		else if (s[i] == ')')
-			--c;
-	return i == len && c == 0;
+	stack = malloc(len / 2);
+	assert(stack != NULL);
+	for (i = 0; i < len; ++i) {
+		if (is_opening(s, i)) {
+			if (top == len / 2)
+				break;
+			stack[top++] = s[i];
+		}
+		if (is_closing(s, i) &&
+		    (top == 0 || !is_same(stack[--top], s[i])))
+			break;
+	}
+	free(stack);
+
+	return i == len && top == 0;
 }
 
+/* Checks if expression is valid. */
 static bool expr_is_valid(const char *s)
 {
 	return brackets_seq_is_valid(s);
@@ -52,12 +91,13 @@ static bool expr_is_valid(const char *s)
  *  - float number
  *  - (expression)
  *  - |expression|
+ *  - [expression]
  *  - sqrt(expression)
  *  - element followed by ^ and an element
  *
  *  Length of the element is stored in `offset'
  */
-static double element_eval(const char *s, int *offset)
+static double element_eval(const char *s, int *offset, bool *error)
 {
 	double  num;
 	char   *endptr;
@@ -67,10 +107,12 @@ static double element_eval(const char *s, int *offset)
 
 	assert(offset != NULL);
 
-	if (s[0] == '(' || s[0] == '|') {
-		num = calc(s + 1, &o);
+	if (is_opening(s, 0)) {
+		num = calc(s + 1, &o, error);
 		if (s[0] == '|')
 			num = fabs(num);
+		if (s[0] == '[')
+			num = trunc(num);
 		++o;
 	} else {
 		num = strtod(s, &endptr);
@@ -79,21 +121,23 @@ static double element_eval(const char *s, int *offset)
 		if (!is_num) {
 			/* We support only `sqrt' as an example. */
 			if (strncmp("sqrt", s, 4) == 0) {
-				num = element_eval(s + 4, &o);
-				assert(num >= 0);
-				num = sqrt(num);
+				num = element_eval(s + 4, &o, error);
+				if (!*error && num < 0)
+					*error = true;
+				else
+					num = sqrt(num);
 				o += 4;
 			} else {
 				assert(false); /* Invalid expression */
 			}
 		}
 	}
-	if (s[o] == '^') {
+	if (!*error && s[o] == '^') {
 		/*
 		 * In this way we reduce complexity of calc().
 		 * Evaluate expression `x^y' as a single element.
 		 */
-		num = pow(num, element_eval(&s[o + 1], &o_pow));
+		num = pow(num, element_eval(&s[o + 1], &o_pow, error));
 		o += o_pow + 1;
 	}
 
@@ -105,7 +149,7 @@ static double element_eval(const char *s, int *offset)
  * Recursive function of evaluation. Function stops on `)' or "end of line".
  * Length of evaluated part (including `)') is stored in `offset'.
  */
-static double calc(const char *s, int *offset)
+static double calc(const char *s, int *offset, bool *error)
 {
 	double  left  = 0;
 	double  right = 0;
@@ -143,29 +187,28 @@ static double calc(const char *s, int *offset)
 	 */
 
 	i = 0;
-	while (s[i] != '\0' && s[i] != ')' && (s[i] != '|' || i == 0)) {
+	while (s[i] != '\0' && !is_closing(s, i)) {
 		/* Take a number */
-		num = element_eval(&s[i], &o);
+		num = element_eval(&s[i], &o, error);
 		i += o;
+		if (*error)
+			break;
 
 		/* After a number we allow only operations and "the end" */
-		assert(s[i] == '+' || s[i] == '-' || s[i] == '*' ||
-		       s[i] == '/' || s[i] == ')' || s[i] == '|' ||
-		       s[i] == '\0');
+		assert(is_op(s[i]) || is_closing(s, i) || s[i] == '\0');
 
 		/* *,/ have higher priority */
 		if (rop == '*') {
 			right *= num;
-			rop = 0;
 		} else if (rop == '/') {
 			if (num == 0) {
-				fprintf(stderr, "Division by zero\n");
-				exit(1);
+				*error = true;
+				break;
 			}
 			right /= num;
-			rop = 0;
 		} else
 			right = num;
+		rop = 0;
 
 		/* Perform lower priority operations */
 		if (s[i] != '*' && s[i] != '/') {
@@ -184,27 +227,34 @@ static double calc(const char *s, int *offset)
 	}
 
 	if (offset != NULL) {
-		assert(s[i] == ')' || s[i] == '|');
+		assert(is_closing(s, i) || *error);
 		*offset = i + 1;
 	}
 	return left;
 }
 
-double eval(const char *expr)
+double eval(const char *expr, bool *error)
 {
-	return calc(expr, NULL);
+	*error = false;
+	return calc(expr, NULL, error);
 }
 
 int main()
 {
-	char   expr[N];
-	double res;
+	char    expr[N];
+	char   *s;
+	double  res;
+	bool    error;
 
-	scanf("%s", expr);
+	s = gets(expr);
+	assert(s == expr);
 	normalize(expr);
 	assert(expr_is_valid(expr));
-	res = eval(expr);
-	printf("%lf\n", res);
+	res = eval(expr, &error);
+	if (error)
+		printf("NaN\n");
+	else
+		printf("%lf\n", res);
 
 	return 0;
 }
